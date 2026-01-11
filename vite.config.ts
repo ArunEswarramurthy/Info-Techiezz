cd81b74e057b01011a512b9d972fa48b0349f9af
ArunEswarramurthy <your_github_email@example.com>
2026-01-11 20:03:03 +0530

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Info-Techiezz-main/',
+  base: '/Info-Techiezz/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
